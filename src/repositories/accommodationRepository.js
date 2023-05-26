2c9ecca8e009592b89fd0f9a5cd09ddf71550184
Bianca Dias <bianca.dias@ufv.br>
2023-05-26 17:03:13 -0300

    correção em formatos de entrega da api

--- src/repositories/accommodationRepository.js
+++ src/repositories/accommodationRepository.js
@@ -1,7 +1,30 @@
 import { db } from "../database/database.connection.js";
 
 export async function getAccommodationsByCityDB(id){
-    return db.query(`SELECT * FROM accommodation WHERE city = $1;`, [id]);
+    //return db.query(`SELECT * FROM accommodation WHERE city = $1;`, [id]);
+    return db.query(`
+    SELECT
+      accommodation.id,
+      accommodation.name,
+      accommodation.city,
+      accommodation.price,
+      accommodation.description,
+      (
+        SELECT ARRAY_AGG(images.image)
+        FROM images
+        WHERE images.id_accommodation = accommodation.id
+      ) AS images,
+      (
+        SELECT ARRAY_AGG(convenience.name)
+        FROM accommodation_convenience
+        JOIN convenience ON accommodation_convenience.id_convenience = convenience.id
+        WHERE accommodation_convenience.id_accommodation = accommodation.id
+      ) AS conveniences
+    FROM
+      accommodation
+    WHERE
+      accommodation.city = $1;
+  `, [id]);
 }
 
 export async function getAccommodationsByIdDB(id){
--- src/repositories/ticketsRepository.js
+++ src/repositories/ticketsRepository.js
@@ -1,9 +1,20 @@
 import { db } from "../database/database.connection.js";
 
 export async function getTicketsByCityDB(id){
-    return db.query(`SELECT * FROM tickets WHERE city_dest = $1;`, [id]);
+    return db.query(`SELECT t.*, c.name AS city_dest_name, ci.name AS cia_name, ci.logo
+    FROM tickets t
+    JOIN cities c ON c.id = t.city_dest
+    JOIN cias ci ON ci.id = t.cia
+    WHERE c.id = $1;
+    `, [id]);
 }
 
 export async function getTicketsByIdDB(id){
-    return db.query(`SELECT * FROM tickets WHERE id = $1;`, [id]);
+    return db.query(`SELECT t.*, c.name AS city_dest_name, ci.name AS cia_name, ci.logo
+    FROM tickets t
+    JOIN cities c ON c.id = t.city_dest
+    JOIN cias ci ON ci.id = t.cia
+    WHERE t.id = $1;
+    `, [id]);
+
 }
